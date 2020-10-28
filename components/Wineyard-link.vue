46836fd866e6dcbd3122023d734518ccf645772c
Martin <martin_peternel@hotmail.com>
2020-10-28 21:06:33 +0100

    first commit

---
+++ components/Wineyard-link.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="wineyard-link">
+        <img src="" alt="wineyard image">
+        <h2>{{ name }}</h2>
+    </div>
+</template>
+<script>
+export default {
+    name : "wineyardLink",
+    props: [
+        "key",
+        "name",
+        "image"
+    ]
+}
+</script>
+
+<style>
+    div {
+        border: 1px black solid;
+    }
+
+    .wineyards-container,
+    .wineyard-details {
+        width: 100%;
+    }
+
+    .wineyard-details {
+        display: flex;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="wineyards-container">
+        <div class="wineyards">
+            <wineyardLink v-for = "wineyard in wineyards"
+                      v-bind:key = "wineyard.id"
+                      v-bind:image = "wineyard.image"
+                      v-bind:name = "wineyard.name"
+            />
+        </div>
+        <div class="wineyard-details">
+            <div class="container-left">
+                <img src="" alt="wineyard image">
+                <h2></h2>
+            </div>
+            <div class="container-right">
+                <p></p>
+            </div>
+        </div>
+    </div>    
+</template>
+<script>
+import wineyardLink from "@/components/Wineyard-link.vue";
+import json from '@/json/wineyards.json'
+
+export default {
+    components : {
+        wineyardLink
+    },
+    data() {
+      return {
+        wineyards: json
+      }
+    }
+}
+</script>
+<style>
+    div {
+        border: 1px black solid;
+    }
+
+    .wineyards-container,
+    .wineyard-details {
+        width: 100%;
+    }
+
+    .wineyard-details {
+        display: flex;
+    }
+</style>
